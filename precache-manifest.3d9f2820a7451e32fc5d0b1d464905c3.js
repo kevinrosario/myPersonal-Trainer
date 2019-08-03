eaa772e34facc719a81eaed599ae34edd285bf72
kevinrosario <kevin.rosario9@upr.edu>
2019-08-03 00:11:44 -0400

    Updates

---
+++ precache-manifest.3d9f2820a7451e32fc5d0b1d464905c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc3fdad84ddc11d919aed6d3c3d06bd",
+    "url": "/mypersonal-trainer/index.html"
+  },
+  {
+    "revision": "19604b08904f184278cb",
+    "url": "/mypersonal-trainer/static/css/main.19941eeb.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/mypersonal-trainer/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "19604b08904f184278cb",
+    "url": "/mypersonal-trainer/static/js/main.75339b97.chunk.js"
+  },
+  {
+    "revision": "1ce775b0a6f0acc90870",
+    "url": "/mypersonal-trainer/static/js/runtime~main.ba16c41b.js"
+  }
+]);
