330ed3fe8bf8fa564175f42c4156a9217fbff729
kevinrosario <kevin.rosario9@upr.edu>
2019-08-07 15:44:01 -0400

    Updates

--- precache-manifest.9328b1317cbec0e753bd8bb41dc0f3f7.js
+++ precache-manifest.9328b1317cbec0e753bd8bb41dc0f3f7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2cd775de24ad90365c996ecbc9d8e9d3",
+    "revision": "2dff6d9a7905703df9eacec7ca4563c9",
     "url": "/mypersonal-trainer/index.html"
   },
   {
-    "revision": "5d7f99297a7e10120f94",
+    "revision": "be339d9ad1aacc41d8c5",
     "url": "/mypersonal-trainer/static/css/main.f8ca22bb.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mypersonal-trainer/static/js/2.66e1c1e9.chunk.js"
   },
   {
-    "revision": "5d7f99297a7e10120f94",
-    "url": "/mypersonal-trainer/static/js/main.26a2bee2.chunk.js"
+    "revision": "be339d9ad1aacc41d8c5",
+    "url": "/mypersonal-trainer/static/js/main.0f3d489b.chunk.js"
   },
   {
     "revision": "1ce775b0a6f0acc90870",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mypersonal-trainer/precache-manifest.08633cf3fc11fda11a68b683a2a404bf.js"
+  "/mypersonal-trainer/precache-manifest.9328b1317cbec0e753bd8bb41dc0f3f7.js"
 );
 
 self.addEventListener('message', (event) => {
